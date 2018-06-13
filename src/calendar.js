dcec65ac6d3da71142c6c30c3a67c822eac0c1e0
Jeffery Patton <jefferypatton@JeffPattonsMBP.fios-router.home>
2018-06-13 08:01:13 -0400

    Cleanup + minor comments

--- src/calendar.js
+++ src/calendar.js
@@ -17,7 +17,7 @@ export class Calendar {
         eventLocation: '44th',
         startTime: '13:00',
         endTime: '14:00',
-        eventDate: 21,
+        eventDate: 20,
         eventDesc: '',
         eventId: 1},
       {
@@ -45,15 +45,15 @@ export class Calendar {
   addEvent() { 
     this.clearFormFields();
     this.formHead = "Add New Event";
-    this.event.eventId = this.events[this.events.length-1]+1;
-    console.log(this.events[0]['eventDate']);
+    // this.event.eventId = this.events[this.events.length-1]+1; // Still need unique ID format. Will be used for event edit and delete.
     this.events = this.sortEvents();
 
   }
 
   saveEvent(){  
     this.event.eventDate = Number(this.event.eventDate);
-    this.events.push(this.event);
+    let copy = Object.assign({}, this.event); // Fix for a property reference error
+    this.events.push(copy);
     this.clearFormFields();
 
     
@@ -67,7 +67,6 @@ export class Calendar {
 
   editEvent(){
     this.formHead = "View/Edit Event";
-
   }
 
   clearFormFields(){
@@ -80,9 +79,9 @@ export class Calendar {
   sortEvents(){
     return this.events.sort((a,b) => {
       if(a.eventDate === b.eventDate){
-        return a.startTime < b.startTime; //Sort by time
+        return a.startTime > b.startTime; //Sort by time
       }
-      return a.eventDate > b.eventDate;//Sort by day
+      return a.eventDate > b.eventDate; //Sort by day
     });
   }
 }
